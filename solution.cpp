c257075f2a117e38854ef8daf9ad014be0d81bac
Vasiliev-20 <fjbhgyuh12@gmail.com>
2025-10-10 15:57:20 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -3,13 +3,34 @@
 using namespace std;
 
 int main() {
-  string temp;
-  int itemsCount;
-  cin >> temp >> temp >> temp >> itemsCount;
-  if (itemsCount == 1){
-  cout << "Куплен товар за 100 рублей";
-  } else {
-  cout <<  "Куплено 6 штук товара за 2300 рублей";
-  }
-  return 0;
+    string temp;
+    int cost, cnt, itemsCount;
+    long long totalCost = 0, totalItems = 0;
+    
+    cin >> temp >> temp >> temp >> itemsCount;
+    
+    for (int i = 0; i < itemsCount; i++) {
+        while (cin >> temp) {
+            if (temp == "за") {
+                cin >> cost;
+                totalCost += cost;
+                totalItems++;
+                break;
+            }
+            if (temp == "по") {
+                cin >> cost >> temp >> cnt;
+                totalCost += cost * cnt;
+                totalItems += cnt;
+                break;
+            }
+        }
+    }
+    
+    if (totalItems == 1) {
+        cout << "Куплен товар за " << totalCost << " рублей" << endl;
+    } else {
+        cout << "Куплено " << totalItems << " штук товара за " << totalCost << " рублей" << endl;
+    }
+    
+    return 0;
 }
