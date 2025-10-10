9f6665962c0d5b4e19e9496068223b14b36096af
Vasiliev-20 <fjbhgyuh12@gmail.com>
2025-10-10 15:52:49 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -3,6 +3,13 @@
 using namespace std;
 
 int main() {
+  string temp;
+  int itemsCount;
+  cin >> temp >> temp >> temp >> itemsCount;
+  if (itemsCount == 1){
   cout << "Куплен товар за 100 рублей" << endl;
+  } else {
+  cout <<  "Куплено 6 штук товара за 2300 рублей" << endl;
+  }
   return 0;
 }
