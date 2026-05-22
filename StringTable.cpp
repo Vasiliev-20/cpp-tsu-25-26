41f925f1113f6372cd8396510cbfa1dd4316c3d0
Vasiliev-20 <fjbhgyuh12@gmail.com>
2026-05-22 08:08:20 +0300

    Update StringTable.cpp

--- StringTable.cpp
+++ StringTable.cpp
@@ -1,34 +1,38 @@
 #include "StringTable.h"
-#include "Atoms.h"
-#include <algorithm>
 
-const std::string& StringTable::operator[](int index) const {
-    return _strings[index];
+const std::string& StringTable::operator[](const int index) const {
+    if (index < 0 || index >= strings_.size()) {
+        throw std::out_of_range("Index is out of range");
+    }
+    return strings_[index];
 }
-
 std::shared_ptr<StringOperand> StringTable::add(const std::string& name) {
-    auto it = std::find(_strings.begin(), _strings.end(), name);
-    if (it != _strings.end()) {
-        int index = static_cast<int>(it - _strings.begin());
-        if (index < static_cast<int>(_operands.size()) && _operands[index]) {
-            return _operands[index];
-        }
-        auto operand = std::make_shared<StringOperand>(index, this);
-        if (index >= static_cast<int>(_operands.size()))
-            _operands.resize(index + 1);
-        _operands[index] = operand;
-        return operand;
+    auto it = std::find(strings_.begin(), strings_.end(), name);
+    int index;
+    if (it != strings_.end()) {
+        index = std::distance(strings_.begin(), it);
     }
-    int newIndex = static_cast<int>(_strings.size());
-    _strings.push_back(name);
-    auto operand = std::make_shared<StringOperand>(newIndex, this);
-    _operands.push_back(operand);
-    return operand;
+    else {
+        strings_.push_back(name);
+        index = strings_.size() - 1;
+    }
+    return std::make_shared<StringOperand>(index, this);
 }
 
-std::ostream& operator<<(std::ostream& os, const StringTable& st) {
-    for (size_t i = 0; i < st._strings.size(); ++i) {
-        os << i << " " << st._strings[i] << std::endl;
+void StringTable::generateStrings(std::ostream& stream) const {
+    for (int i = 0; const auto& str : strings_) {
+        stream << "str" << i << ": " << "DB " << str << ", 0\n";
+        ++i;
     }
+}
+
+std::ostream& operator<<(std::ostream& os, const StringTable& table) {
+    os << "STRING TABLE" << std::endl;
+    os << std::setfill('-') << std::setw(50) << "-" << std::endl;
+    os << std::setfill(' ');
+    for (size_t i = 0; i < table.strings_.size(); ++i) {
+        os << std::left << std::setw(8) << i << std::setw(8) << table.strings_[i] << std::endl;
+    }
+    os << std::endl;
     return os;
 }
