cb24894a3998c41fe5d23c9d0e51c6a51c0254e8
Vasiliev-20 <fjbhgyuh12@gmail.com>
2026-06-16 16:01:16 +0300

    Update Scaner.cpp

--- Scaner.cpp
+++ Scaner.cpp
@@ -1,7 +1,7 @@
 #include "Scaner.h"
-#include <iostream>
-#include <string>
-#include <map>
+#include <cctype>   // для isspace, isdigit, isalpha, isalnum
+
+// ---------- Реализация Token ----------
 
 Token::Token(LexemType type) : type_(type), value_(0), str_("") {}
 Token::Token(int value) : type_(LexemType::num), value_(value), str_("") {}
@@ -10,100 +10,93 @@ Token::Token(char c) : type_(LexemType::chr), value_((int)c), str_("") {}
 Token::Token(int value, const std::string& str) : type_(LexemType::error), value_(value), str_(str) {}
 
 std::string Token::LexemTypeToString_(LexemType type) {
-	switch (type) {
-	case LexemType::num: return "num";
-	case LexemType::chr: return "chr"; 
-    case LexemType::str: return "str";
-    case LexemType::id: return "id";
-
-    case LexemType::lpar: return "lpar";
-    case LexemType::rpar: return "rpar";
-    case LexemType::lbrace: return "lbrace";
-    case LexemType::rbrace: return "rbrace";
-    case LexemType::lbracket: return "lbracket";
-    case LexemType::rbracket: return "rbracket";
-
-    case LexemType::semicolon: return "semicolon";
-    case LexemType::comma: return "comma";
-    case LexemType::colon: return "colon";
-
-    case LexemType::opassign: return "opassign";
-    case LexemType::opplus: return "opplus";
-    case LexemType::opminus: return "opminus";
-    case LexemType::opmult: return "opmult";
-    case LexemType::opinc: return "opinc";
-    case LexemType::opeq: return "opeq";
-    case LexemType::opne: return "opne";
-    case LexemType::oplt: return "oplt";
-    case LexemType::opgt: return "opgt";
-    case LexemType::ople: return "ople";
-    case LexemType::opnot: return "opnot";
-    case LexemType::opor: return "opor";
-    case LexemType::opand: return "opand";
-
-    case LexemType::kwint: return "kwint";
-    case LexemType::kwchar: return "kwchar";
-    case LexemType::kwif: return "kwif";
-    case LexemType::kwelse: return "kwelse";
-    case LexemType::kwswitch: return "kwswitch";
-    case LexemType::kwcase: return "kwcase";
-    case LexemType::kwdefault: return "kwdefault";
-    case LexemType::kwwhile: return "kwwhile";
-    case LexemType::kwfor: return "kwfor";
-    case LexemType::kwreturn: return "kwreturn";
-    case LexemType::kwin: return "kwin";
-    case LexemType::kwout: return "kwout";
-
-    case LexemType::eof: return "eof";
-    case LexemType::error: return "error";
+    switch (type) {
+        case LexemType::num: return "num";
+        case LexemType::chr: return "chr";
+        case LexemType::str: return "str";
+        case LexemType::id: return "id";
+        case LexemType::lpar: return "lpar";
+        case LexemType::rpar: return "rpar";
+        case LexemType::lbrace: return "lbrace";
+        case LexemType::rbrace: return "rbrace";
+        case LexemType::lbracket: return "lbracket";
+        case LexemType::rbracket: return "rbracket";
+        case LexemType::semicolon: return "semicolon";
+        case LexemType::comma: return "comma";
+        case LexemType::colon: return "colon";
+        case LexemType::opassign: return "opassign";
+        case LexemType::opplus: return "opplus";
+        case LexemType::opminus: return "opminus";
+        case LexemType::opmult: return "opmult";
+        case LexemType::opinc: return "opinc";
+        case LexemType::opeq: return "opeq";
+        case LexemType::opne: return "opne";
+        case LexemType::oplt: return "oplt";
+        case LexemType::opgt: return "opgt";
+        case LexemType::ople: return "ople";
+        case LexemType::opnot: return "opnot";
+        case LexemType::opor: return "opor";
+        case LexemType::opand: return "opand";
+        case LexemType::kwint: return "kwint";
+        case LexemType::kwchar: return "kwchar";
+        case LexemType::kwif: return "kwif";
+        case LexemType::kwelse: return "kwelse";
+        case LexemType::kwswitch: return "kwswitch";
+        case LexemType::kwcase: return "kwcase";
+        case LexemType::kwdefault: return "kwdefault";
+        case LexemType::kwwhile: return "kwwhile";
+        case LexemType::kwfor: return "kwfor";
+        case LexemType::kwreturn: return "kwreturn";
+        case LexemType::kwin: return "kwin";
+        case LexemType::kwout: return "kwout";
+        case LexemType::eof: return "eof";
+        case LexemType::error: return "error";
+        default: return "unknown";
     }
-
-    return "unknown";
 }
 
 void Token::print(std::ostream& stream) {
-	stream << "[" << LexemTypeToString_(type_);
+    stream << "[" << LexemTypeToString_(type_);
     switch (type_) {
         case LexemType::num:
             stream << ", " << value_;
             break;
-
         case LexemType::chr:
             stream << ", '" << (char)value_ << "'";
             break;
-
         case LexemType::id:
         case LexemType::str:
         case LexemType::error:
             stream << ", \"" << str_ << "\"";
             break;
-
         default:
             break;
     }
     stream << "]" << std::endl;
 }
 
-LexemType Token::type() {
-	return type_;
-}
+LexemType Token::type() { return type_; }
+int Token::value() { return value_; }
+std::string Token::str() { return str_; }
 
-int Token::value() {
-	return value_;
-}
+// ---------- Глобальные словари (оставлены как есть) ----------
 
-std::string Token::str(){
-	return str_;
-}
-
-std::map<char, LexemType> punctuation{ {'(', LexemType::lpar}, {')', LexemType::rpar}, {'{', LexemType::lbrace},
-    {'}', LexemType::rbrace}, {'[', LexemType::lbracket}, {']', LexemType::rbracket}, {';', LexemType::semicolon},
-    {',', LexemType::comma}, {':', LexemType::colon}
+std::map<char, LexemType> punctuation{
+    {'(', LexemType::lpar}, {')', LexemType::rpar},
+    {'{', LexemType::lbrace}, {'}', LexemType::rbrace},
+    {'[', LexemType::lbracket}, {']', LexemType::rbracket},
+    {';', LexemType::semicolon}, {',', LexemType::comma},
+    {':', LexemType::colon}
 };
 
-std::map<std::string, LexemType> keywords{ {"int", LexemType::kwint}, {"char", LexemType::kwchar}, {"if", LexemType::kwif},
-    {"else", LexemType::kwelse}, {"switch", LexemType::kwswitch}, {"case", LexemType::kwcase}, {"default", LexemType::kwdefault}, {"while", LexemType::kwwhile},
-    {"for", LexemType::kwfor}, {"return", LexemType::kwreturn}, {"in", LexemType::kwin}, {"out", LexemType::kwout}
+std::map<std::string, LexemType> keywords{
+    {"int", LexemType::kwint}, {"char", LexemType::kwchar},
+    {"if", LexemType::kwif}, {"else", LexemType::kwelse},
+    {"switch", LexemType::kwswitch}, {"case", LexemType::kwcase},
+    {"default", LexemType::kwdefault},
+    {"while", LexemType::kwwhile}, {"for", LexemType::kwfor},
+    {"return", LexemType::kwreturn},
+    {"in", LexemType::kwin}, {"out", LexemType::kwout}
 };
 
 std::map<int, std::string> errors{
@@ -116,94 +109,61 @@ std::map<int, std::string> errors{
     {7, "unclosed symbolic constant"}
 };
 
+// ---------- Реализация Scaner ----------
+
 Scaner::Scaner(std::istream& stream) : stream_(stream) {}
 
 Token Scaner::getNextLexem() {
-    State state = State0;
-    int value = 0;
-    std::string buffer = "";
+    State st = State0;          // раньше было state
+    int val = 0;                // раньше value
+    std::string buf = "";       // раньше buffer
 
     for (;;) {
-        char c = stream_.get();
-        switch (state) {
+        char ch = stream_.get();
+        switch (st) {
             case State0:
                 if (!stream_.good()) {
                     return Token(LexemType::eof);
                 }
-                else if (isspace(c)) {
+                if (isspace(ch)) {
                     continue;
                 }
-                else if (punctuation.count(c)) {
-                    return Token(punctuation[c]);
+                if (punctuation.count(ch)) {
+                    return Token(punctuation[ch]);
                 }
-                else if (c == '-') {
-                    return Token(LexemType::opminus);
-                }
-                else if (c == '>') {
-                    return Token(LexemType::opgt);
-                }
-                else if (c == '*') {
-                    return Token(LexemType::opmult);
-                }
-                else if (isdigit(c)) {
-                    value = c - '0';
-                    state = State1;
+                if (ch == '-') return Token(LexemType::opminus);
+                if (ch == '>') return Token(LexemType::opgt);
+                if (ch == '*') return Token(LexemType::opmult);
+                if (isdigit(ch)) {
+                    val = ch - '0';
+                    st = State1;
                     continue;
                 }
-                else if (isalpha(c) || c == '_') {
-                    buffer.clear();
-                    buffer = c;
-                    state = State5;
+                if (isalpha(ch) || ch == '_') {
+                    buf.clear();
+                    buf = ch;
+                    st = State5;
                     continue;
                 }
-                else if (c == '!') {
-                    state = State7;
-                    continue;
-                }
-                else if (c == '\'') {
-                    state = State2;
-                    continue;
-                }
-                else if (c == '\"') {
-                    buffer.clear();
-                    state = State4;
-                    continue;
-                }
-                else if (c == '=') {
-                    state = State9;
-                    continue;
-                }
-                else if (c == '<') {
-                    state = State8;
-                    continue;
-                }
-                else if (c == '+') {
-                    state = State10;
-                    continue;
-                }
-                else if (c == '|') {
-                    state = State11;
-                    continue;
-                }
-                else if (c == '&') {
-                    state = State12;
-                    continue;
-                }
-                else {
-                    return Token(1, errors[1]);
-                }
-                break;
+                if (ch == '!') { st = State7; continue; }
+                if (ch == '\'') { st = State2; continue; }
+                if (ch == '\"') { buf.clear(); st = State4; continue; }
+                if (ch == '=') { st = State9; continue; }
+                if (ch == '<') { st = State8; continue; }
+                if (ch == '+') { st = State10; continue; }
+                if (ch == '|') { st = State11; continue; }
+                if (ch == '&') { st = State12; continue; }
+                return Token(1, errors[1]);
 
             case State1:
                 if (!stream_.good()) {
-                    return Token(value);
+                    return Token(val);
                 }
-                if (isdigit(c)) {
-                    value = value * 10 + (c - '0');
-                }
-                else {
+                if (isdigit(ch)) {
+                    val = val * 10 + (ch - '0');
+                } else {
                     stream_.unget();
-                    return Token(value);
+                    return Token(val);
                 }
                 break;
 
@@ -211,137 +171,84 @@ Token Scaner::getNextLexem() {
                 if (!stream_.good()) {
                     return Token(7, errors[7]);
                 }
-                if (c == '\'') {
+                if (ch == '\'') {
                     return Token(4, errors[4]);
-                }
-                else {
-                    value = c;
-                    state = State3;
+                } else {
+                    val = ch;
+                    st = State3;
                 }
                 break;
-            
+
             case State3:
                 if (!stream_.good()) {
                     return Token(7, errors[7]);
                 }
-                if (c == '\'') {
-                    state = State0;
-                    return Token((char)value);
-                }
-                else {
+                if (ch == '\'') {
+                    st = State0;
+                    return Token((char)val);
+                } else {
                     return Token(5, errors[5]);
                 }
-                
                 break;
 
             case State4:
                 if (!stream_.good()) {
                     return Token(6, errors[6]);
-                } 
-                else if (c == '\"') {
-                    return Token(LexemType::str, buffer);
                 }
-                else {
-                    buffer += c;
-                    state = State4;
+                if (ch == '\"') {
+                    return Token(LexemType::str, buf);
+                } else {
+                    buf += ch;
                 }
                 break;
 
             case State5:
                 if (!stream_.good()) {
-                    if (keywords.count(buffer)) {
-                        return Token(keywords[buffer]);
-                    }
-                    return Token(LexemType::id, buffer);
+                    if (keywords.count(buf)) return Token(keywords[buf]);
+                    return Token(LexemType::id, buf);
                 }
-                if (isalnum(c) || c == '_') {
-                    buffer += c;
-                    continue;
-                }
-                else {
+                if (isalnum(ch) || ch == '_') {
+                    buf += ch;
+                } else {
                     stream_.unget();
-                    if (keywords.count(buffer)) {
-                        return Token(keywords[buffer]);
-                    }
-                    return Token(LexemType::id, buffer);
+                    if (keywords.count(buf)) return Token(keywords[buf]);
+                    return Token(LexemType::id, buf);
                 }
                 break;
 
             case State7:
-                if (!stream_.good()) {
-                    return Token(LexemType::opnot);
-                }
-                if (c == '=') {
-                    return Token(LexemType::opne);
-                }
-                else {
-                    stream_.unget();
-                    return Token(LexemType::opnot);
-                }
-                break;
+                if (!stream_.good()) return Token(LexemType::opnot);
+                if (ch == '=') return Token(LexemType::opne);
+                stream_.unget();
+                return Token(LexemType::opnot);
 
             case State8:
-                if (!stream_.good()) {
-                    return Token(LexemType::oplt);
-                }
-                if (c == '=') {
-                    return Token(LexemType::ople);
-                }
-                else {
-                    stream_.unget();
-                    return Token(LexemType::oplt);
-                }
-                break;
+                if (!stream_.good()) return Token(LexemType::oplt);
+                if (ch == '=') return Token(LexemType::ople);
+                stream_.unget();
+                return Token(LexemType::oplt);
 
             case State9:
-                if (!stream_.good()) {
-                    return Token(LexemType::opassign);
-                }
-                if (c == '=') {
-                    return Token(LexemType::opeq);
-                }
-                else {
-                    stream_.unget();
-                    return Token(LexemType::opassign);
-                }
-                break;
+                if (!stream_.good()) return Token(LexemType::opassign);
+                if (ch == '=') return Token(LexemType::opeq);
+                stream_.unget();
+                return Token(LexemType::opassign);
 
             case State10:
-                if (!stream_.good()) {
-                    return Token(LexemType::opplus);
-                }
-                if (c == '+') {
-                    return Token(LexemType::opinc);
-                }
-                else {
-                    stream_.unget();
-                    return Token(LexemType::opplus);
-                }
-                break;
+                if (!stream_.good()) return Token(LexemType::opplus);
+                if (ch == '+') return Token(LexemType::opinc);
+                stream_.unget();
+                return Token(LexemType::opplus);
 
             case State11:
-                if (!stream_.good()) {
-                    return Token(2, errors[2]);
-                }
-                if (c == '|') {
-                    return Token(LexemType::opor);
-                }
-                else {
-                    return Token(2, errors[2]);
-                }
-                break;
+                if (!stream_.good()) return Token(2, errors[2]);
+                if (ch == '|') return Token(LexemType::opor);
+                return Token(2, errors[2]);
 
             case State12:
-                if (!stream_.good()) {
-                    return Token(3, errors[3]);
-                }
-                if (c == '&') {
-                    return Token(LexemType::opand);
-                }
-                else {
-                    return Token(3, errors[3]);
-                }
-
+                if (!stream_.good()) return Token(3, errors[3]);
+                if (ch == '&') return Token(LexemType::opand);
+                return Token(3, errors[3]);
         }
     }
 }
